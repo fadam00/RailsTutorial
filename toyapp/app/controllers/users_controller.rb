b13803144525a9b1298ac9cfea4819edbde51b3e
Frank Adames <frankv.adames@outlook.com>
2016-10-05 18:16:30 -0400

    Created associations between microposts and users tables, and added validations to microposts model

--- toyapp/app/controllers/users_controller.rb
+++ toyapp/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  validates :content, length: {maximum: 140}
   # GET /users
   # GET /users.json
   def index
