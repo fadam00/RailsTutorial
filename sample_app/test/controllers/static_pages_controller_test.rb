12a2e43d9772bfd814f70a79168cec7d7f6e9d7b
Frank Adames <frankv.adames@outlook.com>
2016-10-06 17:52:41 -0400

    Refactored test controller

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+	def setup
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    asset_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    asset_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-   	asset_select "title", "Home | Ruby on Rails Tutorial Sample App"
-
+   	asset_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
   	get static_pages_about_url
   	assert_response :success
-  	asset_select "title", "Home | Ruby on Rails Tutorial Sample App"
-
+  	asset_select "title", "About | #{@base_title}
   end
 
 
