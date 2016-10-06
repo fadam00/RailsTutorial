6578ef970c118494bfe7d4c528ece42d352b8736
Frank Adames <frankv.adames@outlook.com>
2016-10-06 18:40:25 -0400

    Passed tests for 'should get contact'

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -24,5 +24,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   	assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+  	get static_pages_contact_url
+  	assert_response :success
+  	assert_select "title", "Contact | #{@base_title}"
+  	assert_select "h1", "Contact"
+  end
+
+
 
 end
