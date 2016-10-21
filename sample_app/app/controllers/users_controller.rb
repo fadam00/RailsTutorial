33f76ae2e4e3e7f5604474aa6bfad5da74d4c18a
Frank Adames <frankv.adames@outlook.com>
2016-10-21 10:23:14 -0400

    Added authorization

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -45,6 +45,13 @@ private
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
 
 
 end
