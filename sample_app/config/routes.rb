61a70c7f28a5919bf0c98b2eb9e6da77524a30bf
Frank Adames <frankv.adames@outlook.com>
2016-10-20 20:59:36 -0400

    Added seassions testing and sessionhelper module for login

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -19,9 +19,10 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
 
-  get '/login', to: 'sessions#create'
+  post '/login', to: 'sessions#create'
 
   get '/logout', to: 'sessions#destroy'
+
 
 
   resources :users
