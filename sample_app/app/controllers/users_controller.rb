8abdc1aa95ca540c9b7bee2a2e4d42b397ca0dcc
Frank Adames <frankv.adames@outlook.com>
2016-10-24 14:29:09 -0400

    Done

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :show, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
 
   def new
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -41,6 +42,21 @@ class UsersController < ApplicationController
       end
     end
 
+    def following
+      @title = "Following"
+      @user = User.find(params[:id])
+      @users = @user.followers.paginate(page: params[:page])
+      render 'show_follow'
+    end
+
+    def followers
+      @title = "Followers"
+      @user = User.find(params[:id])
+      @users = @user.followers.paginate(page: params[:page])
+      render 'show_follow'
+    end
+    
+
     
 
 
@@ -48,15 +64,6 @@ private
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
-
-    #Confirms logged in user
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
 
 #Confirms correct user
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -23,9 +23,15 @@ Rails.application.routes.draw do
 
   delete '/logout', to: 'sessions#destroy'
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
 
-  resources :users
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
