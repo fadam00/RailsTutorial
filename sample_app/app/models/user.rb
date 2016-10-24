2eaf2158f907913567fd64031bce803cdf45e944
Frank Adames <frankv.adames@outlook.com>
2016-10-24 09:54:11 -0400

    Added followers(passive relationships) to user model

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
 									foreign_key: "follower_id",
 									dependent: :destroy
 
+	has_many :passive_relationships, class_name: "Relationship",
+									foreign_key: "followed_id",
+									dependent: :destroy
+
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
+
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	before_save {email.downcase!}
@@ -18,4 +25,20 @@ class User < ApplicationRecord
 		def feed
 			Micropost.where("user_id = ?", id)
 		end
+
+		#Follows a user.
+		def follow(other_user)
+			active_relationships.create(followed_id: other_user.id)
+		end
+
+		#Unfollows a user.
+		def unfollow(other_user)
+			active_relationships.find_by(followed_id: other_user.id).destroy
+		end
+
+		#Returns true if the current user is following the other user.
+		def following?(other_user)
+			following.include?(other_user)
+		end
+
 end
