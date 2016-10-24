108720197b5e8aaefc006c2781eeaf7a1d038164
Frank Adames <frankv.adames@outlook.com>
2016-10-23 20:15:41 -0400

    Added validations for picture file type and size

--- sample_app/app/models/micropost.rb
+++ sample_app/app/models/micropost.rb
@@ -5,5 +5,11 @@ class Micropost < ApplicationRecord
 validates :user_id, presence: true
 validates :content, presence: true, length: { maximum: 140 }
 
+private
 
+	def picture_size
+		if picture.size > 5.megabytes
+			errors.add(:picture, "should be less than 5MB")
+		end
+	end
 end
